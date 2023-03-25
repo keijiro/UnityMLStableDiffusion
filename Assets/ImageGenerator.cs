cc20493f077f99f6cb558ed71b9d39f259f36aba
Keijiro Takahashi <keijiro@gmail.com>
2023-03-25 11:27:36 +0900

    Better UI and async operations

--- Assets/ImageGenerator.cs
+++ Assets/ImageGenerator.cs
@@ -3,59 +3,89 @@ using UnityEngine.UI;
 
 public sealed class ImageGenerator : MonoBehaviour
 {
+    #region Editable attributes
+
     [SerializeField] Texture2D _source = null;
     [SerializeField] float _strength = 0.5f;
-    [SerializeField] string _prompt = "a photo of a dog";
     [SerializeField] int _stepCount = 25;
     [SerializeField] int _seed = 100;
     [SerializeField] float _guidanceScale = 8;
-    [SerializeField] RawImage _preview = null;
 
-    StableDiffusion.Pipeline _pipeline;
-    Awaitable _task;
+    [SerializeField] RawImage _uiPreview = null;
+    [SerializeField] InputField _uiPrompt = null;
+    [SerializeField] Button _uiGenerate = null;
+    [SerializeField] Text _uiMessage = null;
+
+    #endregion
+
+    #region Stable Diffusion pipeline objects
 
     string ResourcePath
       => Application.streamingAssetsPath + "/StableDiffusion";
 
-    async Awaitable RunGeneratorAsync(byte[] image)
+    StableDiffusion.Pipeline _pipeline;
+
+    #endregion
+
+    #region Async operations
+
+    async Awaitable SetUpPipelineAsync()
     {
+        _uiMessage.text = "Loading model data...";
+        _uiGenerate.interactable = false;
+
         await Awaitable.BackgroundThreadAsync();
 
-        if (_pipeline == null)
-            _pipeline = StableDiffusion.Pipeline.Create(ResourcePath);
+        _pipeline = StableDiffusion.Pipeline.Create(ResourcePath);
 
-        _pipeline.SetConfig(_prompt, _stepCount, _seed, _guidanceScale);
+        await Awaitable.MainThreadAsync();
+
+        _uiMessage.text = "";
+        _uiGenerate.interactable = true;
+    }
+
+    async Awaitable RunPipelineAsync()
+    {
+        var image = _source != null ? _source.GetRawTextureData() : null;
+        _pipeline.SetConfig(_uiPrompt.text, _stepCount, _seed, _guidanceScale);
+
+        _uiMessage.text = "Generating...";
+        _uiGenerate.interactable = false;
+
+        await Awaitable.BackgroundThreadAsync();
 
         if (image != null)
             _pipeline.RunGeneratorFromImage(image, _strength);
         else
             _pipeline.RunGenerator();
+
+        await Awaitable.MainThreadAsync();
+
+        var tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
+        tex.LoadRawTextureData(_pipeline.ImageBufferPointer, 512 * 512 * 3);
+        tex.Apply();
+
+        _uiMessage.text = "";
+        _uiGenerate.interactable = true;
+        _uiPreview.texture = tex;
     }
+
+    #endregion
+
+    #region UI callback
+
+    public void OnClickGenerate()
+      => RunPipelineAsync();
+
+    #endregion
+
+    #region MonoBehaviour implementation
 
     void Start()
-      => _task = RunGeneratorAsync
-           (_source != null ? _source.GetRawTextureData() : null);
+      => SetUpPipelineAsync();
 
     void OnDestroy()
-    {
-        _task?.Cancel();
-        _task = null;
+      => _pipeline?.Dispose();
 
-        _pipeline?.Dispose();
-        _pipeline = null;
-    }
-
-    void Update()
-    {
-        if (_task?.IsCompleted ?? false)
-        {
-            var tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
-            tex.LoadRawTextureData(_pipeline.ImageBufferPointer, 512 * 512 * 3);
-            tex.Apply();
-
-            _preview.texture = tex;
-
-            _task = null;
-        }
-    }
+    #endregion
 }
